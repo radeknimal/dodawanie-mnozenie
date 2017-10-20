87988ad7c5b98da35f7080aff8691e20b4f4d90e
Radek <radek.nimal@gmail.com>
2017-10-20 15:18:04 +0200

    dzialajacy kalkulator

---
+++ prog2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int mnoz(int a, int b){
+	printf("Wynik mnozenia: %i\n", a * b);
+	return a * b; }
+
+int dodaj(int a, int b){
+	printf("Wynik dodawania: %i\n", a + b);
+	return a + b; }
+
+int odej(int a, int b){
+	printf("Wynik odejmowania: %i\n", a - b);
+	return a - b; }
+
+int dziel(int a, int b){
+	printf("Wynik dzielenia: %i\n", a / b);
+	return a / b;}
+
+int main(void)
+{
+
+	int a,b;
+	char wybor;
+	printf("Jestem kalkulatorem liczb calkowitych \n");
+	printf("Podaj wartosc pierwszej liczby: "); scanf("%i", &a);
+	printf("Podaj znak działania matematycznego: ");scanf(" %c", &wybor);
+	printf("Podaj wartosc drugiej liczby: "); scanf("%i", &b);
+
+	if (wybor == '*'){
+		mnoz(a,b);
+	}else if(wybor == '+'){
+		dodaj(a,b);
+	}else if(wybor == '-'){
+		odej(a, b);
+	}else if(wybor == '/'){
+		dziel (a,b);
+	}else {
+	printf("Nie znam takiego dzialania! \n");
+	}
+return 0;
+}
+
+
