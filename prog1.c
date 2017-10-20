4fca649f6fbe3eac7a6d23147e27ceb8930359df
Radek <radek.nimal@gmail.com>
2017-10-20 14:14:03 +0200

    kalkulator z funkcja przeniesiona

--- prog1.c
+++ prog1.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
-int mnoz(int a, int b){return a * b; }
-int dodaj(int a, int b){return a + b; }
-int odej(int a, int b){return a - b; }
-int dziel(int a, int b){return a / b;}
+int mnoz(int a, int b){
+	printf("Wynik mnozenia: %i\n", a*b);
+	return a * b; }
+
+int dodaj(int a, int b){
+	printf("Wynik dodawania: %i\n", a+b);
+	return a + b; }
+
+int odej(int a, int b){
+	printf("Wynik odejmowania: %i\n", a-b);
+	return a - b; }
+
+int dziel(int a, int b){
+	printf("Wynik dzielenia: %i\n", a/b);
+	return a / b;}
+
 int main(void)
 {
 	printf("Jestem kalkulatorem dla liczb 2 i 5\nPodaj znak pozadanego dzialania matematycznego: \n");
@@ -11,14 +23,10 @@ int main(void)
 	char wybor;
 	scanf ("%c", &wybor);
 	if (wybor == '*')
-	{
-	printf("Wynik mnozenia: %i\n", mnoz (a,b));
-	}else if(wybor == '+'){
-	printf("Wynik dodawania: %i\n", dodaj (a,b));
-	}else if(wybor == '-'){
-	printf("Wynik odejmowania: %i\n", odej (a, b));
-	}else if(wybor == '/'){
-	printf("Wynik dzielenia: %i\n", dziel (a, b));
+	{mnoz(a,b);
+	}else if(wybor == '+'){dodaj (a,b);
+	}else if(wybor == '-'){odej (a, b);
+	}else if(wybor == '/'){dziel (a,b);
 	}else {
 	printf("Nie znam takiego dzialania! \n");
 }
